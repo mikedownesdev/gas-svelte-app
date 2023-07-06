7eae2157e0eaccce76937e42a6d668644c0495b0
Mike Downes <mikedownes.dev@gmail.com>
2023-07-06 16:03:38 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./pages/Home.svelte"; 
+  import Settings from "./pages/Settings.svelte"
+
+  export let url = "/";
+</script>
+
+<Router {url}>
+  <nav>
+      <Link to="/">Home</Link>
+      <Link to="/settings">Settings</Link>
+  </nav>
+  <div>
+      <Route path="/"><Home /></Route>
+      <Route path="/settings"><Settings /></Route>
+  </div>
+</Router>
