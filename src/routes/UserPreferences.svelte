9a853696ff49cf65dba8383315a7302e9c2ee211
Mike Downes <mikedownes.dev@gmail.com>
2023-07-11 16:11:20 -0400

    basic user settings page start

---
+++ src/routes/UserPreferences.svelte
@@ -0,0 +1,24 @@
+<div>
+    <div class="card bg-base-300 shadow-xl">
+        <div class="card-body">
+            <h2 class="card-title">User Preferences</h2>
+            <p class="text-gray-500">Modify your user preferences here. Remember to save!</p>
+            <div class="divider"></div>
+            <div class="form-control w-full max-w-xs">
+                <label class="label">
+                  <span class="label-text">First Name</span>
+                </label>
+                <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+            </div>
+            <div class="form-control w-full max-w-xs">
+                <label class="label">
+                  <span class="label-text">Last Name</span>
+                </label>
+                <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+            </div>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Save</button>
+            </div>
+        </div>
+      </div>
+</div>
