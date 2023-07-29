bfc452734c0d847377a186ce4d5211a8e7b0729a
Mike Downes <mikedownes.dev@gmail.com>
2023-07-29 13:24:06 -0400

    fixed navigating between views locally

--- src/routes/View.svelte
+++ src/routes/View.svelte
@@ -1,67 +1,25 @@
 <script>
+    import { onMount,afterUpdate, beforeUpdate, onDestroy } from 'svelte';
     import LoadingSpinner from "../components/LoadingSpinner.svelte";
     import runGas from "../lib/runGas.js";
     
+    /** @type {string} */
     export let id;
 
+    /** @type {boolean} */
     let loading = false;
+
+    /** @type {View} */
     let viewData = undefined;
 
-    // const viewConfiguration = {
-    //     spreadsheetId: '1XD1RVZsXw6DLQv_ksIdzuYdHPcsbpXhml94nMAz5_vA',
-    //     gid: '0',
-    //     datasourceType: 'Google Sheets',
-    //     pageSize: 10,
-    //     columns: [
-    //         {
-    //             label: 'Full Name',
-    //             name: 'fullName',
-    //             type: 'string',
-    //             width: 100,
-    //             format: 'text',
-    //             filter: {
-    //                 type: 'text',
-    //                 value: '',
-    //             },
-    //             sort: {
-    //                 type: 'text',
-    //                 value: '',
-    //             },
-    //         },
-    //         {
-    //             label: 'Job',
-    //             name: 'job',
-    //             type: 'string',
-    //             width: 100,
-    //             format: 'text',
-    //             filter: {
-    //                 type: 'text',
-    //                 value: '',
-    //             },
-    //             sort: {
-    //                 type: 'text',
-    //                 value: '',
-    //             },
-    //         },
-    //         {
-    //             label: 'Favorite Color',
-    //             name: 'favoriteColor',
-    //             type: 'string',
-    //             width: 100,
-    //             format: 'text',
-    //             filter: {
-    //                 type: 'text',
-    //                 value: '',
-    //             },
-    //             sort: {
-    //                 type: 'text',
-    //                 value: '',
-    //             },
-    //         },
-    //     ],
-    // }
+    // viewData is fetched and persisted when the id prop changes
+    $: fetchViewData(id);
 
-    async function fetchViewData() {
+    /**
+     * Fetches the view data from the server.
+     * @param {string} id
+     */
+    async function fetchViewData(id) {
         loading = true;
         
         console.log('fetching view data...')
@@ -79,9 +37,6 @@
                 loading = false;
             });
     }
-
-    fetchViewData();
-
 </script>
 
 <h1>View #{id}</h1>
@@ -102,7 +57,7 @@
         </thead>
         <tbody>
             
-            {#each viewData.data.records as record}
+            {#each viewData.queryResult.records as record}
             <tr>
                 <th>X</th>
                 {#each viewData.configuration.fields as field}
