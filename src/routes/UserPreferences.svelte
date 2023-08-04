bcee18891374e835b35082b365895012537f1214
Mike Downes <mikedownes.dev@gmail.com>
2023-08-04 09:58:12 -0400

    User Preferences UI update

--- src/routes/UserPreferences.svelte
+++ src/routes/UserPreferences.svelte
@@ -1,5 +1,6 @@
 <script>
     import LoadingSpinner from "../components/LoadingSpinner.svelte";
+    import Panel from "../components/Panel.svelte";
     import runGas from "../lib/runGas.js";
 
     /** @type {boolean} */
@@ -15,63 +16,63 @@
         try {
             let preferencesObject = {
                 firstName: userPreferences.firstName,
-                lastName: userPreferences.lastName
+                lastName: userPreferences.lastName,
             };
             userPreferences = await submitUserPreferences(preferencesObject);
         } catch (err) {
-            console.error('handleClick completed with error', err);
+            console.error("handleClick completed with error", err);
         }
     }
 
     async function submitUserPreferences(preferencesObject) {
         loading = true;
 
-        console.log('submitting user preferences...', preferencesObject)
-        
-        runGas('setUserPreferences', [preferencesObject])
+        console.log("submitting user preferences...", preferencesObject);
+
+        runGas("setUserPreferences", [preferencesObject])
             .then((result) => {
                 userPreferences = result;
-                console.log('User preferences:', userPreferences);
+                console.log("User preferences:", userPreferences);
             })
             .catch((err) => {
-                console.error('Error submitting user preferences', err);
+                console.error("Error submitting user preferences", err);
             })
             .finally(() => {
-                console.log('User preferences submitted.');
+                console.log("User preferences submitted.");
                 loading = false;
             });
     }
 
     async function fetchUserPreferences() {
         loading = true;
-        
-        console.log('fetching user preferences...')
-        
-        runGas('getUserPreferences')
+
+        console.log("fetching user preferences...");
+
+        runGas("getUserPreferences")
             .then((result) => {
                 userPreferences = result;
-                console.log('User preferences:', userPreferences);
+                console.log("User preferences:", userPreferences);
             })
             .catch((err) => {
-                console.error('Could not get user preferences:', err);
+                console.error("Could not get user preferences:", err);
             })
             .finally(() => {
-                console.log('User preferences loaded.');
+                console.log("User preferences loaded.");
                 loading = false;
             });
     }
 
-    if (!userPreferences) { fetchUserPreferences(); }
+    if (!userPreferences) {
+        fetchUserPreferences();
+    }
 </script>
 
 <div>
-    <div class="card bg-base-300 shadow-xl">
-        {#if loading}
-            <LoadingSpinner />
-        {/if}
-        {#if userPreferences}
-        <div class="card-body">
-            <h2 class="card-title">User Preferences for {userPreferences?.firstName} {userPreferences?.lastName}</h2>
+    {#if loading}
+        <LoadingSpinner />
+    {/if}
+    {#if userPreferences}
+        <Panel title="User Preferences">
             <p class="text-gray-500">
                 Modify your user preferences here. Remember to save!
             </p>
@@ -102,9 +103,9 @@
             </div>
             <div class="card-actions justify-end">
                 <button on:click={handleClick} class="btn btn-primary"
-                    >Save</button>
+                    >Save</button
+                >
             </div>
-        </div>
-        {/if}
-    </div>
+        </Panel>
+    {/if}
 </div>
