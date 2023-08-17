1734c8cf5bc708ab16dcd71e6afab838255c7ce8
Mike Downes <mikedownes.dev@gmail.com>
2023-08-17 12:56:09 -0400

    fixed build step
    Installed the `rollup-plugin-delete` plugin to remove purge the
    `dist/server` files before building. This way we can have a better
    synergy between the state of the `src/server` and `dist/server` files.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { viteSingleFile } from 'vite-plugin-singlefile';
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import copy from 'rollup-plugin-copy';
+import del from 'rollup-plugin-delete';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
@@ -9,6 +10,9 @@ export default defineConfig({
   plugins: [
     svelte(),
     viteSingleFile(),
+
+    // Delete the dist/server directory before each build
+    del({ targets: 'dist/server/*' }),
     copy({
       targets: [
         { src: 'src/server', dest: 'dist' }
