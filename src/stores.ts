6cb158ae6c9d5ada194a34fb66452c7bb0912db5
Mike Downes <mikedownes.dev@gmail.com>
2023-09-25 14:55:15 -0400

    Moved appConfig to stores

--- src/stores.ts
+++ src/stores.ts
@@ -1,6 +1,9 @@
 import { writable } from "svelte/store";
 import { UserType } from "./types/schemas";
+import { AppConfigurationType } from "./types/schemas";
 
 export const sessionUser = writable<UserType | null>(null);
 
 export const isLoading = writable<boolean>(false);
+
+export const appConfiguration = writable<AppConfigurationType | null>(null);
