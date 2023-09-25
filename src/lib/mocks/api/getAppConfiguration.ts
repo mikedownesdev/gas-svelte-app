e3a1c7b5ae5158a592210a3d5293127f9cf0bfa6
Mike Downes <mikedownes.dev@gmail.com>
2023-09-25 14:51:49 -0400

    update to GET mocks

--- src/lib/mocks/api/getAppConfiguration.ts
+++ src/lib/mocks/api/getAppConfiguration.ts
@@ -1,12 +1,17 @@
 import sleep from "../sleep";
 import { appConfiguration } from "../data/appConfiguration";
 import { AppConfigurationType } from "../../../types/schemas";
+import { getAdmins_ } from "../getAdmins_";
 
 export async function getAppConfiguration(): Promise<AppConfigurationType> {
   sleep();
 
-  /** @type {AppConfiguration} */
-  let mockResponse = appConfiguration;
+  let appConfig = {
+    ...appConfiguration,
+    admins: getAdmins_(),
+  };
+
+  let mockResponse = appConfig;
 
   console.log("mockResponse", mockResponse);
 
