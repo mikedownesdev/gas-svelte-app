fa7aaddf91dc28db45db602e134898b11d3ecd9c
Mike Downes <mikedownes.dev@gmail.com>
2023-09-26 13:02:45 -0400

    ensuring user is authenticated before app load

--- src/server/doGet.js
+++ src/server/doGet.js
@@ -1,4 +1,12 @@
 function doGet(e) {
+
+  // We shouldn't load the application if we aren't able to get the user's
+  // identity. In this case, we return the noAuth.html page.
+  let activeUserEmail = Session.getActiveUser().getEmail();
+  if (activeUserEmail === "") { return HtmlService.createTemplateFromFile('noAuth').evaluate() }
+
+  // Otherwise, we check to see if this application has been initialized.
+  // If not, we do so now
   let appConfig = loadAppConfiguration_();
   if (!appConfig) {
     initializeApp();
