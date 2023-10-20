27389b7c987215780360087b4d130aff2c9c7df0
Mike Downes <mikedownes.dev@gmail.com>
2023-10-20 13:17:04 -0400

    removed views/datasources from mocks

--- src/lib/mocks/data/appConfiguration.ts
+++ src/lib/mocks/data/appConfiguration.ts
@@ -1,10 +1,6 @@
-import { dataSourceConfigurations } from "./dataSourceConfigurations";
-import { viewConfigurations } from "./viewConfigurations";
 import { user } from "./user";
 
 export let appConfiguration = {
   appName: "Mock App",
   deployingUserEmail: user.email,
-  dataSourceConfigurations,
-  viewConfigurations,
 };
