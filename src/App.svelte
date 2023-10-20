c5b5753c7f52bc594a0de67a87b6d0e0fe6cffdd
Mike Downes <mikedownes.dev@gmail.com>
2023-10-20 11:42:08 -0400

    no view links, out of scope right now

--- src/App.svelte
+++ src/App.svelte
@@ -170,28 +170,6 @@
             User Preferences
           </NavLink>
           <div class="divider" />
-          {#if appConfiguration}
-            {#each $appConfiguration?.viewConfigurations as view}
-              <NavLink to="/view/{view.id}" onClick={toggleDrawer}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-5 h-5"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3"
-                  />
-                </svg>
-
-                {view.label}</NavLink
-              >
-            {/each}
-          {/if}
         </ul>
       </div>
     </div>
